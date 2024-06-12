4db1c794f1a15f90c66097a5496a6538ead1394a
Mohammad Ismail <ismailalam901@gmail.com>
2024-06-12 17:57:32 +0600

    photot section updated

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/PhotoGallery.scss';
 
 const PhotoGallery = () => {
     const [data, setData] = useState([]);
+    const [showFullDescription, setShowFullDescription] = useState({});
 
     useEffect(() => {
         fetch('/dummyData.json')
             .then(response => response.json())
             .then(data => setData(data));
     }, []);
+
+    const toggleDescription = (id) => {
+        setShowFullDescription(prevState => ({
+            ...prevState,
+            [id]: !prevState[id]
+        }));
+    };
 
     return (
         <div className="photo-gallery container">
@@ -20,19 +27,35 @@ const PhotoGallery = () => {
                         <div className="photo-item">
                             {item.type === 'multiple' ? (
                                 <div className="photo-grid">
-                                    {item.photos.map((photo, index) => (
+                                    {item.photos.slice(0, 4).map((photo, index) => (
                                         <div className="photo" key={index}>
                                             <img src={photo} alt={`Photo ${index + 1}`} className="img-fluid" />
+                                            {index === 3 && item.photos.length > 4 && (
+                                                <div className="more">+{item.photos.length - 4}</div>
+                                            )}
                                         </div>
                                     ))}
-                                    <div className="more">+{item.more}</div>
                                 </div>
                             ) : (
                                 <div className="photo-single">
                                     <img src={item.photos[0]} alt="Single photo" className="img-fluid" />
                                 </div>
                             )}
-                            {item.description && <p className="description">{item.description}</p>}
+                            {item.description && (
+                                <div className="description-container">
+                                    <p className={`description ${showFullDescription[item.id] ? 'full' : ''}`}>
+                                        {showFullDescription[item.id]
+                                            ? item.description
+                                            : `${item.description.slice(0, 53)}...`}
+                                    </p>
+                                    <span
+                                        className="toggle-description"
+                                        onClick={() => toggleDescription(item.id)}
+                                    >
+                                        {showFullDescription[item.id] ? 'সংক্ষেপ করুন' : 'আরো পড়ুন'}
+                                    </span>
+                                </div>
+                            )}
                         </div>
                     </div>
                 ))}
