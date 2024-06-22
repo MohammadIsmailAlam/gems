43fd992ea819d481a7164f04f438930d51915a4f
Mohammad Ismail <ismailalam901@gmail.com>
2024-06-22 23:50:47 +0600

    first design applied

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -19,46 +19,57 @@ const PhotoGallery = () => {
     };
 
     return (
-        <div className="photo-gallery container">
-            <h2 className="section-title">ফটো গ্যালারি</h2>
+        <div className="container sectional-wrapper">
             <div className="row">
-                {data.map(item => (
-                    <div className="col-md-4 mb-4" key={item.id}>
-                        <div className="photo-item">
-                            {item.type === 'multiple' ? (
-                                <div className="photo-grid">
-                                    {item.photos.slice(0, 4).map((photo, index) => (
-                                        <div className="photo" key={index}>
-                                            <img src={photo} alt={`Photo ${index + 1}`} className="img-fluid" />
-                                            {index === 3 && item.photos.length > 4 && (
-                                                <div className="more">+{item.photos.length - 4}</div>
-                                            )}
-                                        </div>
-                                    ))}
+                <div className="col-md-12">
+                    <div className="dynamicDiv" id="dd.0.2.0">
+                        <div className="row">
+                            <div className="col-md-12 pagingBorder">
+                                <h2 className="section-title">ফটো গ্যালারি</h2>
+                                <div id="theImageGrid">
+                                    <ul className="row imagegrid" id="imagegrid">
+                                        {data.map(item => (
+                                            <li className="col-xs-12 col-sm-6 col-md-4" key={item.id}>
+                                                <div className="imgHolder">
+                                                    <div className="fauxCrop">
+                                                        {item.type === 'multiple' ? (
+                                                            item.photos.slice(0, 4).map((photo, index) => (
+                                                                <a href="#" key={index}>
+                                                                    <img src={photo} alt={`Photo ${index + 1}`} className="grid-image img-responsive" />
+                                                                </a>
+                                                            ))
+                                                        ) : (
+                                                            <a href="#">
+                                                                <img src={item.photos[0]} alt="Single photo" className="grid-image img-responsive" />
+                                                            </a>
+                                                        )}
+                                                    </div>
+                                                </div>
+                                                {item.description && (
+                                                    <div className="description-container">
+                                                        <p className={`description ${showFullDescription[item.id] ? 'full' : ''}`}>
+                                                            {showFullDescription[item.id]
+                                                                ? item.description
+                                                                : `${item.description.slice(0, 53)}...`}
+                                                        </p>
+                                                        <span
+                                                            className="toggle-description"
+                                                            onClick={() => toggleDescription(item.id)}
+                                                        >
+                                                            {showFullDescription[item.id] ? 'সংক্ষেপ করুন' : 'আরো পড়ুন'}
+                                                        </span>
+                                                    </div>
+                                                )}
+                                                <div className="clearfix"></div>
+                                            </li>
+                                        ))}
+                                    </ul>
                                 </div>
-                            ) : (
-                                <div className="photo-single">
-                                    <img src={item.photos[0]} alt="Single photo" className="img-fluid" />
-                                </div>
-                            )}
-                            {item.description && (
-                                <div className="description-container">
-                                    <p className={`description ${showFullDescription[item.id] ? 'full' : ''}`}>
-                                        {showFullDescription[item.id]
-                                            ? item.description
-                                            : `${item.description.slice(0, 53)}...`}
-                                    </p>
-                                    <span
-                                        className="toggle-description"
-                                        onClick={() => toggleDescription(item.id)}
-                                    >
-                                        {showFullDescription[item.id] ? 'সংক্ষেপ করুন' : 'আরো পড়ুন'}
-                                    </span>
-                                </div>
-                            )}
+                            </div>
                         </div>
                     </div>
-                ))}
+                    {/* end sample */}
+                </div>
             </div>
         </div>
     );
